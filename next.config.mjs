dd9fcd85831321f332cef1aa5ee23aaea9dec78b
BNGVISHU <bngvishu@gmail.com>
2026-07-08 12:30:10 +0530

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  allowedDevOrigins: ['192.168.29.147'],
+};
+
+export default nextConfig;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar flex flex-row gap-3 bg-neutral-900 p-7 max-sm:p-3 fixed w-full '>
+      <div className='w-full text-center'>
+        <h1 className={"geo-font text-4xl text-red-800 max-sm:text-3xl "}>ASUROVISHVA</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
